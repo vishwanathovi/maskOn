93f119a15851475373711d8176e892d6aa748094
vishwanathovi <vishwanathovi@gmail.com>
2020-09-28 14:56:26 +0530

    added upload image functions

--- src/client/App.js
+++ src/client/App.js
@@ -1,37 +1,87 @@
-import React, { Component } from 'react';
-import './app.css';
-import ReactImage from './react.png';
+import React, { Component } from "react";
+import "./app.css";
+import ReactImage from "./react.png";
+
+import axios from "axios";
 
 export default class App extends Component {
   state = { username: null };
 
   componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
+    fetch("/api/getUsername")
+      .then((res) => res.json())
+      .then((user) => this.setState({ username: user.username }));
   }
+
+  onFileChange = (event) => {
+    // Update the state
+    this.setState({ selectedFile: event.target.files[0] });
+  };
+
+  // On file upload (click the upload button)
+  onFileUpload = () => {
+    // Create an object of formData
+    const formData = new FormData();
+
+    // Update the formData object
+    formData.append(
+      "myFile",
+      this.state.selectedFile,
+      this.state.selectedFile.name
+    );
+
+    // Details of the uploaded file
+    console.log(this.state.selectedFile);
+
+    // Request made to the backend api
+    // Send formData object
+    axios.post("api/uploadfile", formData);
+  };
+
+  // File content to be displayed after
+  // file upload is complete
+  fileData = () => {
+    if (this.state.selectedFile) {
+      return (
+        <div>
+          <h2>File Details:</h2>
+          <p>File Name: {this.state.selectedFile.name}</p>
+          <p>File Type: {this.state.selectedFile.type}</p>
+          <p>
+            Last Modified:{" "}
+            {this.state.selectedFile.lastModifiedDate.toDateString()}
+          </p>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <br />
+          <h4>Choose before Pressing the Upload button</h4>
+        </div>
+      );
+    }
+  };
 
   render() {
     const { username } = this.state;
     return (
       <div>
-      <h1> MaskON </h1>
+        <h1> MaskON </h1>
         <div className="upload-main">
-        <h3> Upload Your Own Image </h3>
-        <div>
-                <input type="file" onChange={this.onFileChange} />
-                <button onClick={this.onFileUpload}>
-                  Upload!
-                </button>
-            </div>
+          <h3> Upload Your Own Image </h3>
+          <div>
+            <input type="file" onChange={this.onFileChange} />
+            <button onClick={this.onFileUpload}>Upload!</button>
+          </div>
+          {this.fileData()}
         </div>
         <div className="webcam-main">
-        <h3> Start Webcam </h3>
+          <h3> Start Webcam </h3>
         </div>
         <div className="default-images-main">
-        <h3> Choose from a default list of Images </h3>
+          <h3> Choose from a default list of Images </h3>
         </div>
-
       </div>
     );
   }
