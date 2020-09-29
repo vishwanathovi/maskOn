d60c06a92d51e486b67d6ac745766d51bdbd0733
vishwanathovi <vishwanathovi@gmail.com>
2020-09-29 17:35:51 +0530

    working image upload

--- src/client/App.js
+++ src/client/App.js
@@ -4,46 +4,65 @@ import ReactImage from "./react.png";
 
 import axios from "axios";
 import * as tf from "@tensorflow/tfjs";
+import Webcam from "react-webcam";
 
 export default class App extends Component {
-  state = { username: null };
+  state = { username: null, model: null };
 
   componentDidMount() {
     fetch("/api/getUsername")
       .then((res) => res.json())
-      .then((user) => this.setState({ username: user.username }));
+      .then((user) => this.setState({ username: user.username, photoURL: "" }));
   }
 
   loadModel = async () => {
-    const model = await tf.loadLayersModel(
+    let model = await tf.loadLayersModel(
       "http://localhost:8080/data/model.json" // Define server URL as a constant
     );
+
+    this.setState({ model });
     console.log("model loaded successfully");
+  };
+
+  predictImage = async () => {
+    const { model } = this.state;
+    const img = document.getElementById("display-img");
+
+    // console.log(img);
+    let maxPixel = tf.scalar(255);
+    let tensor = tf.browser
+      .fromPixels(img)
+      .resizeNearestNeighbor([150, 150])
+      .toFloat()
+      .div(maxPixel)
+      .expandDims();
+
+    // console.log("tensor: ", tensor.toString());
+
+    model
+      .predict(tensor)
+      .data()
+      .then((predictions) => {
+        let maskFlag = "";
+        if (predictions < 0.5) {
+          maskFlag = "With Mask";
+        } else {
+          maskFlag = "Without Mask";
+        }
+        console.log(maskFlag);
+      });
   };
 
   onFileChange = (event) => {
     // Update the state
-    this.setState({ selectedFile: event.target.files[0] });
-  };
+    var photo = event.target.files[0];
+    const photoURL = URL.createObjectURL(photo);
 
-  // On file upload (click the upload button)
-  onFileUpload = () => {
-    // Create an object of formData
-    const formData = new FormData();
+    this.setState({ photoURL, photo }, () => {
+      // this.predictImage();
+    });
 
-    // Update the formData object
-    formData.append(
-      "myFile",
-      this.state.selectedFile,
-      this.state.selectedFile.name
-    );
-
-    // Details of the uploaded file
-    console.log(this.state.selectedFile);
-
-    // Request made to the backend api
-    // Send formData object
-    axios.post("api/uploadfile", formData);
+    // this.predictImage(img);
   };
 
   // File content to be displayed after
@@ -72,7 +91,7 @@ export default class App extends Component {
   };
 
   render() {
-    const { username } = this.state;
+    const { username, photoURL } = this.state;
     return (
       <div>
         <h1> MaskON </h1>
@@ -81,12 +100,24 @@ export default class App extends Component {
           <h3> Upload Your Own Image </h3>
           <div>
             <input type="file" onChange={this.onFileChange} />
-            <button onClick={this.onFileUpload}>Upload!</button>
           </div>
           {this.fileData()}
+          <div>
+            <img
+              id="display-img"
+              ref={(ref) => {
+                this.photoref = ref;
+              }}
+              src={photoURL}
+              width="500px"
+              height="500px"
+              onLoad={this.predictImage}
+            />
+          </div>
         </div>
         <div className="webcam-main">
           <h3> Start Webcam </h3>
+          <Webcam />
         </div>
         <div className="default-images-main">
           <h3> Choose from a default list of Images </h3>
